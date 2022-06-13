e1e94d790a6c74bd8ba98e099ef58563543a4476
ornato-t <tommy.ornato@gmail.com>
2022-06-13 19:19:23 +0200

    Initial commit: working api, readme Ready for heroku deployement

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # flairchange_bot-api
-An API returning data gathered and processed by flairchange_bot
+An API returning data gathered and processed by flairchange_bot.
+
+# Endpoint
+
+`GET /stats`  
+Returns an array of JSON objects containing the flair statistics of r/PoliticalCompassMemes. Each object represents a flair and is composed of two fields: `flair` and `num`.  
+## Example
+`[{`  
+&emsp;`"flair": "Centrist"`  
+&emsp;`"num": "20"`  
+`},`
+`{`  
+&emsp;`"flair": "GreyCentrist"`  
+&emsp;`"num": "40"`  
+`}]`
+
+
+
+# Access
+At the moment the app does not feature any access measure. Instead, as a security precaution, the data is only loaded once per app lifecycle. Any database occurring during the app lifecycle will only be displayed on the next lifecylce.
---
+++ index.js
@@ -0,0 +1,44 @@
+require('dotenv').config()
+
+const uri = process.env.MONGODB_URI
+const MongoClient = require('mongodb').MongoClient
+const client = new MongoClient(uri)
+
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 80
+
+let arr
+
+run()
+
+//Main function
+async function run() {
+    arr = await getStats()
+
+    app.listen(port, () => { //Listener
+        console.log(`Listeing on http://localhost:${port}`)
+    })
+}
+
+//Handler for GET /stats
+app.get('/stats', async(req, res) => {
+    try {
+        console.log('Answering request for:\t/stats')
+        res.send(arr)
+    } catch (err) {
+        console.log(err)
+    }
+})
+
+//Connects to MongoDB and returns the data from the 'stats' collection. Refreshed only once per lifecycle
+async function getStats() {
+    await client.connect()
+    const db = client.db('flairChangeBot').collection('stats')
+
+    let arr = await db.find().toArray()
+
+    client.close()
+
+    return arr
+}
